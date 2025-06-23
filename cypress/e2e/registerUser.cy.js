7ce776a97011233815cb35939b91697df7afa9e0
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-23 12:15:03 +0300

    updated file structure

---
+++ cypress/e2e/registerUser.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+beforeEach(() => {
+    cy.baseurl()
+})
+describe('Register User', () => {
+    it('Verify that the base url has load successfully', () => {
+        cy.url().should('include', 'automationexercise.com')
+    })
+    
+})
+    
