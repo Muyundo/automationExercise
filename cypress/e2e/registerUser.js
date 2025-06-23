38e9c413b415b82a7d2af5e4f4ebb2ff46935e65
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-23 10:25:35 +0300

    added url verification

--- cypress/e2e/registerUser.js
+++ cypress/e2e/registerUser.js
@@ -1,1 +1,10 @@
 /// <reference types="cypress" />
+beforeEach(() => {
+    cy.baseurl()
+})
+describe('Register User', () => {
+    it('Verify that the base url has load successfully', () => {
+        cy.url().should('include', 'automationexercise.com')
+    })
+})
+    
