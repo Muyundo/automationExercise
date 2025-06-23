89442657ce572e85f6b79576354201bf086a1375
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-23 13:00:22 +0300

    added signup url verification

--- cypress/e2e/registerUser.cy.js
+++ cypress/e2e/registerUser.cy.js
@@ -7,6 +7,12 @@ describe('Register User', () => {
         cy.url().should('include', 'automationexercise.com')
         cy.get('.header-middle > .container > .row').contains(' Signup / Login').click()
         cy.url().should('include', '/login')
+        cy.get('[data-qa="signup-name"]').type('Test User')
+        cy.get('[data-qa="signup-email"]').type('user@example2.com') 
+        cy.get('[data-qa="signup-button"]').click()
+        cy.wait(1000)
+        //cy.contains('Email Address already exists!').should('not.be.visible');
+        cy.url().should('include', '/signup')
     })
 
     
