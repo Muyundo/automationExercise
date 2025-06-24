1600dcbbafd86001c2b6d8ebad8b91278c843d8f
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-24 07:55:51 +0300

    updated email generation and name via faker

--- cypress/e2e/registerUser.cy.js
+++ cypress/e2e/registerUser.cy.js
@@ -1,4 +1,9 @@
 /// <reference types="cypress" />
+
+const { faker } = require("@faker-js/faker")
+
+const email = faker.internet.email()
+const name = faker.person.fullName()
 beforeEach(() => {
     cy.baseurl()
 })
@@ -7,13 +12,17 @@ describe('Register User', () => {
         cy.url().should('include', 'automationexercise.com')
         cy.get('.header-middle > .container > .row').contains(' Signup / Login').click()
         cy.url().should('include', '/login')
-        cy.get('[data-qa="signup-name"]').type('Test User')
-        cy.get('[data-qa="signup-email"]').type('user@example2.com') 
+        cy.get('[data-qa="signup-name"]').type(name)
+        cy.get('[data-qa="signup-email"]').type(email) 
         cy.get('[data-qa="signup-button"]').click()
         cy.wait(1000)
         //cy.contains('Email Address already exists!').should('not.be.visible');
         cy.url().should('include', '/signup')
         cy.contains('Enter Account Information', {timeout: 10000}).should('be.visible')
+
+        const gender = ['#id_gender1', '#id_gender2']
+        const randomIndex = Math.floor(Math.random() * gender.length);
+        cy.get(gender[randomIndex]).check()
     })
 
     
