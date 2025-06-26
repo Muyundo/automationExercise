b47477f53c461ab87acd219891124ce46842ae0e
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-26 15:27:36 +0300

    updated test labelling

--- cypress/e2e/registerUser.cy.js
+++ cypress/e2e/registerUser.cy.js
@@ -16,8 +16,8 @@ const mobile = faker.phone.number()
 beforeEach(() => {
     cy.baseurl()
 })
-describe('Register User', () => {
-    it('Verify that the base url has load successfully', () => {
+describe('Test Cases', () => {
+    it('Register User', () => {
         cy.url().should('include', 'automationexercise.com')
         cy.get('.header-middle > .container > .row').contains(' Signup / Login').click()
         cy.url().should('include', '/login')
